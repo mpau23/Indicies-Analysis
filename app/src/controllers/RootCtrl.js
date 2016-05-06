37eddb44d17f7174cd560fcd9d61e0a3f6cfde8f
Miren Pau <miren_pau23@hotmail.co.uk>
2016-05-06 12:32:41 +0100

    Adding weekly options with start date param

--- app/src/controllers/RootCtrl.js
+++ app/src/controllers/RootCtrl.js
@@ -1,15 +1,17 @@
 IndiciesAnalysis.controller('RootCtrl', ['$scope', '$http', '$q', 'MarketDataService', 'CalendarDataService', 'OptionCalculationService',
     function($scope, $http, $q, MarketDataService, CalendarDataService, OptionCalculationService) {
 
-        renderMarketData(15, "SPY", 0);
+        renderMarketData("SPY", 15, 0, "monthly", 2000);
 
         $scope.marketKeys = ['SPY', 'FTSE', 'GDAXI', 'SLV', 'QQQ', 'IWM'];
 
+        $scope.optionTypes = ["weekly", "monthly"];
+
         $scope.changeDaysToExpiry = function() {
-            renderMarketData($scope.openBeforeExpiry, $scope.marketKey, $scope.closeBeforeExpiry);
+            renderMarketData($scope.marketKey, $scope.openBeforeExpiry, $scope.closeBeforeExpiry, $scope.optionType, $scope.startYear);
         };
 
-        function renderMarketData(daysToExpiry, marketKey, closeBeforeExpiry) {
+        function renderMarketData(marketKey, daysToExpiry, closeBeforeExpiry, optionType, startYear) {
 
             if (!marketKey) {
                 marketKey = "SPY";
@@ -21,11 +23,19 @@ IndiciesAnalysis.controller('RootCtrl', ['$scope', '$http', '$q', 'MarketDataSer
 
             if (!closeBeforeExpiry) {
                 closeBeforeExpiry = 0;
+            }            
+
+            if (!optionType) {
+                optionType = "monthly";
+            }            
+
+            if (!startYear) {
+                startYear = 2000;
             }
 
-            var marketDataPromise = MarketDataService.getDailyMarketData(2000, marketKey);
-            var volatilityDataPromise = MarketDataService.getVolatilityData(2000);
-            var monthlyOptions = CalendarDataService.getMonthlyOptions(daysToExpiry, 2000);
+            var marketDataPromise = MarketDataService.getDailyMarketData(startYear, marketKey);
+            var volatilityDataPromise = MarketDataService.getVolatilityData(startYear);
+            var options = CalendarDataService.getOptions(daysToExpiry, startYear, optionType);
 
             $q.all([marketDataPromise, volatilityDataPromise]).then(function(response) {
 
@@ -33,10 +43,10 @@ IndiciesAnalysis.controller('RootCtrl', ['$scope', '$http', '$q', 'MarketDataSer
                 var volatilityData = response[1];
 
                 if (marketData && volatilityData) {
-                    var optionData = OptionCalculationService.getOptionData(marketData, volatilityData, monthlyOptions, closeBeforeExpiry);
+                    var optionData = OptionCalculationService.getOptionData(marketData, volatilityData, options, closeBeforeExpiry);
                 }
 
-                $scope.marketData = monthlyOptions;
+                $scope.marketData = options;
 
             });
         }
--- app/src/services/CalendarDataService.js
+++ app/src/services/CalendarDataService.js
@@ -29,11 +29,43 @@ IndiciesAnalysis.factory('CalendarDataService', ['$http', '$q', function($http, 
             return monthlyExpiresArray;
         },
 
-        getMonthlyOptions: function(daysUntilExpiry, startYear) {
+        getWeeklyExpiries: function(startYear) {
 
-            var monthlyExpiresArray = calendarDataService.getMonthlyExpiries(startYear);
+            var weeklyExpiriesArray = new Array();
+            var today = Date.parse("today");
+            var dates = Date.parse("1-1-" + startYear);
+            var nextFridayOccurrence = dates.moveToNthOccurrence(5, 1);
 
-            angular.forEach(monthlyExpiresArray, function(monthlyExpiry, key) {
+            weeklyExpiriesArray.push({
+                expiry: {
+                    date: nextFridayOccurrence.toString("d-MMM-yyyy")
+                }
+
+            });
+
+            while (nextFridayOccurrence < today) {
+                nextFridayOccurrence.addDays(7);
+                weeklyExpiriesArray.push({
+                    expiry: {
+                        date: nextFridayOccurrence.toString("d-MMM-yyyy")
+                    }
+                });
+            }
+
+            return weeklyExpiriesArray;
+        },
+
+        getOptions: function(daysUntilExpiry, startYear, occurrence) {
+
+            if (occurrence == "weekly") {
+                var expiryArray = calendarDataService.getWeeklyExpiries(startYear);
+            } else {
+                var expiryArray = calendarDataService.getMonthlyExpiries(startYear);
+
+            }
+
+
+            angular.forEach(expiryArray, function(monthlyExpiry, key) {
 
                 var close = Date.parse(monthlyExpiry.expiry.date);
                 close.addDays(-daysUntilExpiry);
@@ -43,7 +75,7 @@ IndiciesAnalysis.factory('CalendarDataService', ['$http', '$q', function($http, 
 
             });
 
-            return monthlyExpiresArray;
+            return expiryArray;
         }
 
     }
